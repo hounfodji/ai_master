953e4e1fb0b9cf129aa4233f8067cc8209947a7c
hounfodji <h.hounfodji@alpi.link>
2025-08-26 11:27:59 +0000

    Simple bot ai agent

---
+++ agents/freecodecamp_langGraph_course/simple_bot.py
@@ -0,0 +1,27 @@
+from typing import List, TypedDict
+from langchain_core.messages import HumanMessage
+from langchain_ollama import OllamaLLM
+from langgraph.graph import StateGraph, START, END
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+    
+llm = OllamaLLM(model="gpt-oss:latest")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f'\nAI: {response}')
+    return state
+
+graph = StateGraph(AgentState)
+
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+user_input = input("Enter your message: ")
+while user_input.lower() != "exit":
+    result = agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter your message: ")
